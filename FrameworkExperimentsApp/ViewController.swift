84c0455df7d0108f79c40b5f3e57d984d34008ca
Hannes Oud <hannes.oud.dev@gmail.com>
2019-05-31 15:25:14 +0200

    Initial Commit

---
+++ FrameworkExperimentsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FrameworkExperimentsApp
+//
+//  Created by Hannes Oud on 31.05.19.
+//  Copyright © 2019 IdeasOnCanvas GmbH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
